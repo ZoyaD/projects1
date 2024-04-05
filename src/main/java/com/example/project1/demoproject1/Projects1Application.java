d828f53f2dd988826f6edccb672b025a0857db46
saiswathid <javadanaboyina@gmail.com>
2024-04-05 17:13:18 -0500

    bla bla bla

---
+++ src/main/java/com/example/project1/demoproject1/Projects1Application.java
@@ -0,0 +1,13 @@
+package com.example.project1.demoproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projects1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projects1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project1/demoproject1/Projects1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project1.demoproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projects1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
